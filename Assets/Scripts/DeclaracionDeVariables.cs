eafb57c055797bbdb0977e4a5dc176ab09309b58
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-03-17 10:41:23 -0300

    Ejercicio 1 terminado

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -7,17 +7,27 @@ public class DeclaracionDeVariables : MonoBehaviour
     int edadUsuario;
     float sueldoUsuario;
     string domicilio;
-    bool estaAprpbado;
+    bool estaAprobado;
 
     // Start is called before the first frame update
     void Start()
-    { 
+    {
+        //inicializo las variables
+        edadUsuario = 45;
+        sueldoUsuario = 1000.00f;
+        domicilio =  "Libertador 6500";
+        estaAprobado = true;
 
+        //escribo las variables en consola
+        Debug.Log(edadUsuario);
+        Debug.Log(sueldoUsuario);
+        Debug.Log(domicilio);
+        Debug.Log(estaAprobado);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
