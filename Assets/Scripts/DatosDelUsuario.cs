12d88c282b51cded739bbaa66313bf7a566b401c
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-03-31 10:00:22 -0300

    ej2 completo

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -12,7 +12,8 @@ public class DatosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Mi nombre es " + nombreUsuario + ", tengo " + edadUsuario
+            + ", mido " + alturaUsuario + " y es " + esDonante + " que soy donante de órganos.");
     }
 
     // Update is called once per frame
