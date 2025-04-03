6982d25b174f91923769c03fb8a05526067fd94d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-04-03 10:15:45 -0300

    Giua 1 Pt 2 Ej 6 Terminado

---
+++ Assets/ParOImpar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParOImpar : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1 > 0)
+        {
+            if(num1 % 2 == 0)
+            {
+                Debug.Log("el número es par");
+            }else
+            {
+                Debug.Log("el número es impar");
+            }
+        }
+        else
+        {
+            Debug.Log("El número debe ser mayor que cero");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
