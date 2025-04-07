99f5ffeca52aa9030492890212a3679a1e1a1e1d
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-04-07 10:42:13 -0300

    algo

--- Assets/Scripts/ConversiondeMoneda.cs
+++ Assets/Scripts/ConversiondeMoneda.cs
@@ -2,50 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConversiondeMoneda : MonoBehaviour
+public class ConversionDeMoneda : MonoBehaviour
 {
+    //1. Declaración de variables
 
     public float montoAConvertir;
     public string codigoMoneda;
 
     float montoConvertido;
-    float montoMinimo = 1000;
+    float montoMinimoAConvertir = 1000;
+
     float cotizacionMonedaElegida;
-    float cotizacionEuro = 1621.3f;
     float cotizacionDolar = 1297.21f;
-    float cotizacionReal = 183.0f;
+    float cotizacionEuro = 1621.12f;
+    float cotizacionReal = 183;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        //2. Ingreso y validación de datos
 
-        if (montoAConvertir < montoMinimo) 
+        if (montoAConvertir < montoMinimoAConvertir)
         {
             Debug.Log("El monto mínimo es $1000");
             return;
         }
-        if (codigoMoneda == "R")
-        {
-            cotizacionMonedaElegida = cotizacionReal;
-            montoConvertido = montoAConvertir / cotizacionMonedaElegida;
-            Debug.Log("el monto convertido seria " + montoConvertido);
-        }
-        else if (codigoMoneda == "E")
+
+        if (codigoMoneda == "E")
         {
             cotizacionMonedaElegida = cotizacionEuro;
-            montoConvertido = montoAConvertir / cotizacionMonedaElegida;
-            Debug.Log("el monto convertido seria " + montoConvertido);
-        }
-        else if (codigoMoneda == "D")
+        }else if (codigoMoneda == "D")
         {
-            cotizacionMonedaElegida = 1297.21f;
-            montoConvertido = montoAConvertir / cotizacionMonedaElegida;
-            Debug.Log("el monto convertido seria " + montoConvertido);
+            cotizacionMonedaElegida = cotizacionDolar ;
+        }else if (codigoMoneda == "R")
+        {
+            cotizacionMonedaElegida = cotizacionReal;
         }
         else
         {
             Debug.Log("Opción de moneda extranjera no válida");
+            return;
         }
+
+        //3. Procesamiento de datos
+
+        montoConvertido = montoAConvertir / cotizacionMonedaElegida;
+
+        //4. Salida de datos
+        Debug.Log("La conversión de ese monto de pesos equivale a " + montoConvertido);
     }
 
     // Update is called once per frame
