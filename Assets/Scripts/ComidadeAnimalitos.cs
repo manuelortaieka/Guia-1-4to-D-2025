cb84a3d7061249a97d967547712b036234b516a2
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-04-07 10:41:25 -0300

    conversion de moneda y comida de animales a la mitad

---
+++ Assets/Scripts/ComidadeAnimalitos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidadeAnimalitos : MonoBehaviour
+{
+    int comidaGatos = 300;
+    int comidaPP = 400;
+    int comidaPG = 700;
+    public string IDanimal;
+    int cantidadComida;
+    public int cantidadDias;
+    float precioAlimento = 0.8f;
+    float precioTotal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cantidadDias < 3)
+        {
+            Debug.Log("ERROR; la cantidad de días debe ser mayor a 3");
+            return;
+        }
+        if (IDanimal == "G")
+        {
+            cantidadComida = comidaGatos * cantidadDias;
+            Debug.Log("Para ese período se necesitan  " + cantidadComida + "g gramos de alimento");
+            Debug.Log("Costo de los alimentos = $" + cantidadComida * precioTotal);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ConversiondeMoneda.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversiondeMoneda : MonoBehaviour
+{
+
+    public float montoAConvertir;
+    public string codigoMoneda;
+
+    float montoConvertido;
+    float montoMinimo = 1000;
+    float cotizacionMonedaElegida;
+    float cotizacionEuro = 1621.3f;
+    float cotizacionDolar = 1297.21f;
+    float cotizacionReal = 183.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (montoAConvertir < montoMinimo) 
+        {
+            Debug.Log("El monto mínimo es $1000");
+            return;
+        }
+        if (codigoMoneda == "R")
+        {
+            cotizacionMonedaElegida = cotizacionReal;
+            montoConvertido = montoAConvertir / cotizacionMonedaElegida;
+            Debug.Log("el monto convertido seria " + montoConvertido);
+        }
+        else if (codigoMoneda == "E")
+        {
+            cotizacionMonedaElegida = cotizacionEuro;
+            montoConvertido = montoAConvertir / cotizacionMonedaElegida;
+            Debug.Log("el monto convertido seria " + montoConvertido);
+        }
+        else if (codigoMoneda == "D")
+        {
+            cotizacionMonedaElegida = 1297.21f;
+            montoConvertido = montoAConvertir / cotizacionMonedaElegida;
+            Debug.Log("el monto convertido seria " + montoConvertido);
+        }
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
